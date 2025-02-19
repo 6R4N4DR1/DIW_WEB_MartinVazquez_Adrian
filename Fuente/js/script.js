6f332f367e7b0b29586b10b79e1591447a9d56ac
GRANADRI <adrianmv25@outlook.es>
2025-02-19 19:13:14 +0100

    fixing and cleaning code

--- Fuente/js/script.js
+++ Fuente/js/script.js
@@ -15,3 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleAside = document.getElementById("toggle-aside");
+    const sidebarTwo = document.getElementById("sidebar-two");
+
+    toggleAside.innerHTML = '<i class="fas fa-angle-down"></i>';
+
+    toggleAside.addEventListener("click", function() {
+        sidebarTwo.classList.toggle("active");
+        toggleAside.classList.toggle("active");
+
+        if (sidebarTwo.classList.contains("active")) {
+            toggleAside.innerHTML = '<i class="fas fa-angle-up"></i>';
+        } else {
+            toggleAside.innerHTML = '<i class="fas fa-angle-down"></i>';
+        }
+    });
+});
